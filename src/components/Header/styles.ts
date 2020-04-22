6b1b87cad2a98e3ce68a71f3a949b3d83c5dd98a
Alan Costa <alancoosta2323@gmail.com>
2020-04-22 10:59:51 -0300

    styles: change header width to max-width.
    To make the header responsive. When decreasing the browser window, the header remained fixed and the "Listing" and "Import" menus were hidden

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -9,7 +9,7 @@ export const Container = styled.div<ContainerProps>`
   padding: 30px 0;
 
   header {
-    width: 1120px;
+    max-width: 1120px;
     margin: 0 auto;
     padding: ${({ size }) => (size === 'small' ? '0 20px ' : '0 20px 150px')};
     display: flex;
